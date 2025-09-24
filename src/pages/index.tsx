524d3f99fbbb06449f671a18a38ef35626da8171
Claudio Bispo <claudio.bispo@ezzeseguros.com.br>
2025-09-24 11:26:32 -0300

    FEAT - Criação da sessão para arquitetura de sites

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Hero from '../components/Hero'
 import DataEngineering from '../components/DataEngineering'
 import PowerBIArchitecture from '../components/PowerBIArchitecture'
+import WebDevelopmentSection from '../components/WebDevelopment'
 
 export default function Home() {
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero/>
       <DataEngineering/>
       <PowerBIArchitecture/>
+      <WebDevelopmentSection/>
     </div>
     
   )
